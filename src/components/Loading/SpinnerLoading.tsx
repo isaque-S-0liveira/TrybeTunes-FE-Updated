052c04b17d8849b421f7e3a0e009f063f32cec96
Isaque Oliveira <zazac3179@gmail.com>
2024-07-10 18:28:09 -0300

    ajuste no tamananho do MusicCard na tela de favoritas

--- src/components/Loading/SpinnerLoading.tsx
+++ src/components/Loading/SpinnerLoading.tsx
@@ -1,6 +1,6 @@
 function SpinnerLoading() {
   return (
-    <div className="spinner-border  text-light" style={ { width: '3rem', height: '3rem' } } role="status">
+    <div className="spinner-border  text-light" role="status">
       <span className="visually-hidden">Loading...</span>
     </div>
   );
