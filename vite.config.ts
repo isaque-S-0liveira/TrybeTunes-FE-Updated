7f3aef00c4b518e5490cb0f4992f0ea0e7a9f3de
Isaque Oliveira <zazac3179@gmail.com>
2024-07-11 23:15:48 -0300

    configurações para deploy

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base:"/TrybeTunes-FE-Updated",
   test: {
     globals: true,
     environment: 'jsdom',
